43da6fe28ab54075d6e0726fc0aeeaa0dfb1565f
Lorenzo Ricci <lorenzo.f.ricci@gmail.com>
2018-09-21 23:53:23 -0300

    popo

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ var modalBtn = document.getElementById('modalBtn');
 
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
+var continueBtn = document.getElementsByClassName('continue')[0];
+
+var infoTab = document.getElementsByClassName('info');
+
+var formTab = document.getElementsByClassName('form');
+
 // open click
 modalBtn.addEventListener('click', openModal);
 
 // close click
 closeBtn.addEventListener('click', closeModal);
+
+continueBtn.addEventListener('click', continuar);
 
 //outside click
 window.addEventListener('click', outsideClick);
@@ -27,6 +35,17 @@ function outsideClick(e) {
     if (e.target == modal) {
         modal.style.display = 'none';
     }
+
+}
+
+function continuar() {
+    infoTab.style.opacity = 0;
+    setTimeout(function(){
+        infoTab.style.display = 'none';
+        formTab.style.opacity = 100;
+        formTab.style.display = 'inline-block';
+    }, 400);
+
 
 }
 
